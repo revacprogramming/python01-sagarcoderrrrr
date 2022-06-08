bab285bbae3cbc8885b089e9aa131d5878c9c12b
sagar <83465839+sagarcoderrrrr@users.noreply.github.com>
2022-06-08 15:25:17 +0000

    ss

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,14 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+file_name = raw_input("Enter file name: ")
+open_file = open(file_name)
+l = list()                      
+for line in open_file:   
+    word= l.rstrip().split()   
+    for i in word:           
+        if i in l:         
+            continue               
+        else :                     
+            l.append(i)       
+l.sort()                         
+print (l)       
